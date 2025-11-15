1101f95e3b28746f68f8a8d0d002611524dc5f39
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-11-15 17:09:01 +0500

    Second Component in React

--- 02-Components/src/App.jsx
+++ 02-Components/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import FirstComponent from './components/FirstComponent'
+import SecondComponent from './components/SecondComponent'
 function App() {
 
   return (
     <>
     <div className="main-container">
     <FirstComponent/>
+    <SecondComponent />
     </div>
     </>
   )
