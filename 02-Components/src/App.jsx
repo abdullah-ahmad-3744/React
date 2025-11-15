61f0b405e2cc99319c633f08ea6ea8adf73b1332
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-11-15 18:29:59 +0500

    Footer Component

--- 02-Components/src/App.jsx
+++ 02-Components/src/App.jsx
@@ -5,18 +5,23 @@ import SecondComponent from './components/SecondComponent'
 import ThirdComponent from './components/ThirdComponent'
 import LoginForm from './components/LoginForm'
 import Header from './components/Header'
+import Footer from './components/Footer'
 function App() {
 
   return (
     <>
     <div className="main-container">
       <Header />
+      {/* Cards */}
       <div className="cards">
         <FirstComponent/>
         <SecondComponent />
         <ThirdComponent />
       </div>
+      {/* Login Fomr */}
     <LoginForm />
+    {/* Footer */}
+    <Footer />
     
     </div>
     </>
---
+++ 02-Components/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import "./Footer.css"
+function Footer () {
+    return (
+        <div className="footer-container">
+            <p>This is Footer</p>
+        </div>
+    )
+}
+export default Footer;
