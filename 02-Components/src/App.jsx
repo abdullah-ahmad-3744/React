5f999be0c6de9c4009e6b3fff9595cf874b435b8
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-11-15 17:52:36 +0500

    LoginForm Component

--- 02-Components/src/App.jsx
+++ 02-Components/src/App.jsx
@@ -3,14 +3,19 @@ import './App.css'
 import FirstComponent from './components/FirstComponent'
 import SecondComponent from './components/SecondComponent'
 import ThirdComponent from './components/ThirdComponent'
+import LoginForm from './components/LoginForm'
 function App() {
 
   return (
     <>
     <div className="main-container">
-    <FirstComponent/>
+      <div className="cards">
+        <FirstComponent/>
     <SecondComponent />
     <ThirdComponent />
+      </div>
+    <LoginForm />
+    
     </div>
     </>
   )
