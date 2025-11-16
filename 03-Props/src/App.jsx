e0e2083dbfe08f27a7006a05e63ee1de15df0844
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-11-16 23:05:41 +0500

    Cards for practicing props

--- 03-Props/src/App.jsx
+++ 03-Props/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
+import FirstComponent from './component/FirstComponent'
+import SecondComponent from './component/SecondComponent'
+import ThirdComponent from './component/ThirdComponent'
 
 function App() {
 
   return (
     <>
     <div className="Main-container">
-      <h1>Props in React</h1>
+      <div className="cards">
+        <FirstComponent />
+        <SecondComponent />
+        <ThirdComponent />
+      </div>
     </div>
     </>
   )
---
+++ 03-Props/src/component/FirstComponent.jsx
@@ -0,0 +1,12 @@
+import "./FirstComponent.css"
+
+function FirstComponent () {
+    return (
+        <div className="first-component">
+            <h1>Card</h1>
+            <h3>Description</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, laborum?</p>
+        </div>
+    )
+}
+export default FirstComponent
