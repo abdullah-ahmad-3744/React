e2f618134250e73329aee84c0671c5c1a794a44e
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-11-18 15:03:13 +0500

    useState() is used to handle the and update the state and rerender the UI to reflect the changes

--- 05-States/src/components/Card.jsx
+++ 05-States/src/components/Card.jsx
@@ -1,18 +1,26 @@
 import "./Card.css"
+import { useState } from "react"
 
 function Card () {
-    function ClickHandler () {
-        console.log ("Added to Cart")
+    const [heading, setHeading] = useState("Card")
+
+
+
+
+    function HeadingHandler () {
+        setHeading("New Heading")
+        console.log("Heading Updated");
+        
     }
 
 
     
     return (
         <div className="card-container">
-            <h2>Card</h2>
+            <h2>{heading}</h2>
             <h3>Description</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, porro.</p>
-            <button onClick={ClickHandler}>Add to Cart</button>
+            <button onClick={HeadingHandler}>Update Heading</button>
         </div>
     )
 }
