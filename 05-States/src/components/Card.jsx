a35e56a01bb63ed15ce4b0d7c92aea5a5784f397
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-11-19 00:46:48 +0500

    Description Updation process

--- 05-States/src/components/Card.jsx
+++ 05-States/src/components/Card.jsx
@@ -3,23 +3,29 @@ import { useState } from "react"
 
 function Card () {
     const [heading, setHeading] = useState("Card")
+   const [description, setDesciption] = useState("This is the default description of the Card...")
 
 
     function HeadingHandler () {
        let user_input = prompt("Enter Card Heading")        
         setHeading(user_input)
         console.log("Heading Updated");
-        
     }
 
-
+    function DescriptionHandler() {
+        let description = prompt("Enter Card Description")
+        setDesciption(description)
+    }
     
     return (
         <div className="card-container">
             <h2>{heading}</h2>
             <h3>Description</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, porro.</p>
-            <button onClick={HeadingHandler}>Update Heading</button>
+            <p>{description}</p>
+            <div className="buttons">
+                <button onClick={HeadingHandler}>Update Heading</button>
+            <button onClick={DescriptionHandler}>Description Handler</button>
+            </div>
         </div>
     )
 }
