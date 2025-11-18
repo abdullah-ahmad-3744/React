c98108f5d057d424155297e9f041f2e7661af4ee
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-11-18 14:56:05 +0500

    A card for practicing the concept of States

--- 05-States/src/App.jsx
+++ 05-States/src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import Card from './components/Card'
 
 function App() {
 
   return (
     <div className="main-container">
-      <h1>States in React</h1>
+      <Card />
+
     </div>
   )
 }
