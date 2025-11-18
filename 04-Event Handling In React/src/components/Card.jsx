54b1d03701544b13905e19f62715fdf58bbb6bd3
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-11-18 13:49:33 +0500

    Props are used as handlers to practice the Event Handling like OnClick etc

--- 04-Event Handling In React/src/components/Card.jsx
+++ 04-Event Handling In React/src/components/Card.jsx
@@ -1,12 +1,18 @@
 import "./Card.css"
 
 function Card () {
+    function ClickHandler () {
+        console.log ("Added to Cart")
+    }
+
+
+    
     return (
         <div className="card-container">
             <h2>Card</h2>
             <h3>Description</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, porro.</p>
-            <button>Add to Cart</button>
+            <button onClick={ClickHandler}>Add to Cart</button>
         </div>
     )
 }
