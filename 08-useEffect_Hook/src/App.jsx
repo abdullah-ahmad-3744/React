6d0775eb0ede303ed3770f97267530e9360e1b34
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-11-26 10:57:41 +0500

    onChange handler on an input filed to track its value

--- 08-useEffect_Hook/src/App.jsx
+++ 08-useEffect_Hook/src/App.jsx
@@ -1,11 +1,19 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [text , setText] = useState('')
+  function ChangeHandler(event) {
+    setText(event.target.value)
+    console.log(text);
+    
+  }
 
   return (
     <div className="main-container">
       <h1>useEffect Hook in React</h1>
+      <input type="text" onChange={ChangeHandler} />
     </div>
   )
 }
