f2b6dc7086c5ff81245ce5e4b84c17ccc20321b7
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-11-26 11:05:52 +0500

    First variation of the useEffect() hook , and this runs on every render

--- 08-useEffect_Hook/src/App.jsx
+++ 08-useEffect_Hook/src/App.jsx
@@ -1,13 +1,22 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [text , setText] = useState('')
+
+  // Frst variation of useEffect() Hook
+  // In this variation code inside the useEffect Hook runs everytime UI is Updated 
+  useEffect( () => {
+    console.log("UI rendering done");
+    
+  })
+
+
+  
   function ChangeHandler(event) {
     setText(event.target.value)
     console.log(text);
-    
   }
 
   return (
