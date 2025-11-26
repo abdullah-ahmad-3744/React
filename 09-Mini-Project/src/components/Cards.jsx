3a618f81985caa5ac9d97fe4d58b4d6266441727
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-11-26 13:04:49 +0500

    API call using fetch() for getting the data of cards from API

--- 09-Mini-Project/src/components/Cards.jsx
+++ 09-Mini-Project/src/components/Cards.jsx
@@ -1,9 +1,23 @@
 import "./Cards.css"
+import { apiUrl } from "../data";
+import { useEffect } from "react";
 
 function Cards () {
+
+
+    useEffect ( () => {
+        async function fetchedData(params) {
+            const apiResponse = await fetch(apiUrl)
+            const jsonData = await apiResponse.json()
+            console.log("Api Data", jsonData);
+            
+        }
+        fetchedData()
+    },[])
+    
     return (
         <div className="cards-container">
-            <h1>Cards</h1>
+
         </div>
     )
 }
