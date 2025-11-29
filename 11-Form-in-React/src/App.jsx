6c745f20ff57f5ad5b65cb5eed08d20de6fc9d8d
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-11-29 21:51:25 +0500

    All the fields or form elements are handled by the single changeHandler efficiently

--- 11-Form-in-React/src/App.jsx
+++ 11-Form-in-React/src/App.jsx
@@ -1,30 +1,51 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  function firstNameChangeHandler (event) {
+
+  const [formData , setFormData] = useState( 
+    {firstName : '', lastName : '', email : ''}
+   )
+
+
+  function changHandler (event) {
+    setFormData( (previousFormData) => {
+      return {
+        ...previousFormData,
+        [event.target.name] :  event.target.value
+      }
+    })
+
     console.log(event.target.value);
     
-  }
 
-  function lastNameChangeHandler (event) {
-    console.log(event.target.value)
   }
+  // function firstNameChangeHandler (event) {
+  //   console.log(event.target.value);
+    
+  // }
 
-  function emailHandler (event) {
-    console.log(event.target.value)
-  }
+  // function lastNameChangeHandler (event) {
+  //   console.log(event.target.value)
+  // }
+
+  // function emailHandler (event) {
+  //   console.log(event.target.value)
+  // }
+
+
   return (
     <div className="main-container">
       <h1>Forms In React </h1>
       <form action="">
-        <input type="text" name="" id="" placeholder='First Name :- ' onChange={firstNameChangeHandler} />
+        <input type="text" name="firstName" id="" value={formData.firstName} placeholder='First Name :- ' onChange={changHandler} />
         <br />
         <br />
-        <input type="text" name="Last Name" id="" placeholder='Last Name: -'  onChange={lastNameChangeHandler}/>
+        <input type="text" name="lastName" id="" value={formData.lastName} placeholder='Last Name: -'  onChange={changHandler}/>
         <br />
         <br />
-        <input type="email" name ="" placeholder='Email:- ' onChange={emailHandler} />
+        <input type="email" name ="email" value={formData.email} placeholder='Email:- ' onChange={changHandler} />
         <br />
       </form>
     </div>
