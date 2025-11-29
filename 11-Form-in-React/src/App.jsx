935b91b634f6b9bcae3d486bc813f922c283bfcc
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-11-29 21:25:05 +0500

    A simple form with few elements and their handlers to capture their value

--- 11-Form-in-React/src/App.jsx
+++ 11-Form-in-React/src/App.jsx
@@ -2,10 +2,31 @@
 import './App.css'
 
 function App() {
+  function firstNameChangeHandler (event) {
+    console.log(event.target.value);
+    
+  }
 
+  function lastNameChangeHandler (event) {
+    console.log(event.target.value)
+  }
+
+  function emailHandler (event) {
+    console.log(event.target.value)
+  }
   return (
     <div className="main-container">
       <h1>Forms In React </h1>
+      <form action="">
+        <input type="text" name="" id="" placeholder='First Name :- ' onChange={firstNameChangeHandler} />
+        <br />
+        <br />
+        <input type="text" name="Last Name" id="" placeholder='Last Name: -'  onChange={lastNameChangeHandler}/>
+        <br />
+        <br />
+        <input type="email" name ="" placeholder='Email:- ' onChange={emailHandler} />
+        <br />
+      </form>
     </div>
   )
 }
