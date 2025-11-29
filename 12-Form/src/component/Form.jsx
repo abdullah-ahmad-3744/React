6f7fe3ea1ac1fa9dd6fd6013f46d37711e210b38
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-11-30 01:26:24 +0500

    Elements are tracked using the event.target in the change handler function

--- 12-Form/src/component/Form.jsx
+++ 12-Form/src/component/Form.jsx
@@ -1,57 +1,79 @@
+import { useState } from "react"
 import "./Form.css"
 
 function Form() {
+    const [formData , setFormData] = useState(
+        { firstName : '', lastName : '',  email : '', country : '', streetAddress : '', city : '', state_province : '', zip_postal_code : '',comments : '', candidates : '',  others : '',  push_notifications : '' })
+
+        function ChangeHandler (event) {
+            console.log(event.target);
+            
+        }
     return (
         <div className="form-container">
             <form action="">
+
                 <label htmlFor="firstName">First Name :</label>
-                <input type="text" name="lastName" id="lastName" />
+                <input type="text" name="firstName" id="firsttName" onChange={ChangeHandler} />
                 <br />
+
                 <label htmlFor="lastName">Last Name : </label>
-                <input type="text" name="lastName" id="lastName" />
+                <input type="text" name="lastName" id="lastName" onChange={ChangeHandler} />
                 <br />
+
                 <label htmlFor="email">Email Address : </label>
-                <input type="email" name = "email" id="email" />
+                <input type="email" name = "email" id="email" onChange={ChangeHandler} />
                 <br />
-                <label htmlFor="dropdown">Country: </label>
-                <select name="drowndown" id="dropdown">
+
+                <label htmlFor="country">Country: </label>
+                <select name="country" id="country"  onChange={ChangeHandler}>
                     <option value="Pakistan">Pakistan</option>
                     <option value="India">India</option>
                     <option value="China">China</option>
                     <option value="Russia">Russia</option>
                     <option value="UAE">UAE</option>
                 </select>
+
+
                 <br />
-                <label htmlFor="street-address">Street Address: </label>
-                <input type="text" name="street-address" id="street-address" />
+                <label htmlFor="streetAddress">Street Address: </label>
+                <input type="text" name="streetAddress" id="streetAddress" onChange={ChangeHandler} />
                 <br />
+
+
                 <label htmlFor="city">City</label>
-                <input type="text" name="city" id="city" />
+                <input type="text" name="city" id="city"  onChange={ChangeHandler}/>
                 <br />
-                <label htmlFor="state/province">State/Province</label>
-                <input type="text" name="state/province" id="state/province" />
+
+
+                <label htmlFor="state_province">State/Province</label>
+                <input type="text" name="state_province" id="state_province" onChange={ChangeHandler} />
                 <br />
-                <label htmlFor="zip/postal-code">ZIP/Postal Code: </label>
-                <input type="text" name="ZIP/Postal-code" id="ZIP/Postal-code" />
+
+
+                <label htmlFor="zip_postal_code">ZIP/Postal Code: </label>
+                <input type="text" name="zip_postal_code" id="zip_postal_code"  onChange={ChangeHandler} />
                 <br />
+
+
                 <label htmlFor="by-email">By Email : </label>
                 <div className="checkboxes">
                     <div className="comments-checkbox">
-                        <input type="checkbox" name="comments" id="comments"  />
+                        <input type="checkbox" name="comments" id="comments"   onChange={ChangeHandler} />
                         <label htmlFor="comments">Comments</label>
                         <div className="text">
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                         </div>
                     </div>
                     <div className="candidates-checkbox">
-                        <input type="checkbox" name="candidates" id="candidates" />
+                        <input type="checkbox" name="candidates" id="candidates"  onChange={ChangeHandler} />
                         <label htmlFor="candidates">Cnadidates</label>
                         <div className="candidates text">
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, magnam.</p>
                         </div>
                     </div>
                     <div className="others-checkbox">
-                        <input type="checkbox" name="others" id="otthers"  />
+                        <input type="checkbox" name="others" id="otthers"  onChange={ChangeHandler}  />
                         <label htmlFor="others">Others</label>
                         <div className="others-text">
                             <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
@@ -59,17 +81,19 @@ function Form() {
                     </div>
                 </div>
 
+
+
                 <div className="push-notifications">
-                    <label htmlFor="push-notifications">Push Notifications</label>
+                    <label htmlFor="push_notifications">Push Notifications</label>
                     <p>Lorem ipsum dolor sit amet consectetur.</p>
                     <div className="radio-buttons">
-                        <input type="radio" name="push-notifications" id="everything" />
+                        <input type="radio" name="push_notifications" id="everything"  onChange={ChangeHandler} />
                         <label htmlFor="push-notifications">Everything</label>
                         <br />
-                        <input type="radio" name="push-notifications" id="same-as-email" />
+                        <input type="radio" name="push_notifications" id="same-as-email"  onChange={ChangeHandler} />
                         <label htmlFor="same-as-email">Same as Email</label>
                         <br />
-                        <input type="radio" name="push-notifications" id="no-push-notifications" />
+                        <input type="radio" name="push_notifications" id="no-push-notifications"  onChange={ChangeHandler} />
                         <label htmlFor="no-push-notifications">No Push Notifications</label>
                     </div>
                 </div>
