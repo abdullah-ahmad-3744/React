7448a840a42570fc852cab49979c3f77c3d59d73
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-02 23:30:00 +0500

    Simple Dark and Light Theme toggler using Context API

--- 17-Context-API/src/App.jsx
+++ 17-Context-API/src/App.jsx
@@ -1,13 +1,27 @@
-
 import './App.css'
 
-function App() {
+import { useContext } from "react";
+import { AppContext} from "./context/AppContext";
 
+function App() {
+  const { theme, setTheme } = useContext(AppContext);
+
+  function DarkModeHandler () {
+    setTheme('dark')
+  }
+
+  function LightModeHandler () {
+    setTheme('')
+  }
   return (
-    <div className="main-container">
-      <h1>Learning Context API</h1>
+    <div className={theme ==='dark' ? ("dark") : ("light")}>
+      <h1>Theme Changer</h1>
+      <div className="buttons">
+        {theme ? (<button  onClick={LightModeHandler}>Light Theme</button>) :
+        (<button  onClick={DarkModeHandler}>Dark Theme</button>)}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
