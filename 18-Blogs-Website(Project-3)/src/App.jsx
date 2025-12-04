fef909093c859ca76644788a187c76f1cf688e6b
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-04 23:28:22 +0500

    API call successfully handled and data is rendring on the UI

--- 18-Blogs-Website(Project-3)/src/App.jsx
+++ 18-Blogs-Website(Project-3)/src/App.jsx
@@ -4,10 +4,15 @@ import Blogs from './components/Blogs'
 import Header from './components/Header'
 import Pagination from './components/Pagination'
 import { AppContext } from './context/AppContextProvider'
+import { useEffect } from 'react'
 
 function App() {
   const {fetchBlogsData} = useContext(AppContext)
-  // fetchBlogsData()
+
+
+  useEffect ( () => {
+    fetchBlogsData()
+  }, [])
   return (
     <div className="main-container">
       <Header />
--- 18-Blogs-Website(Project-3)/src/components/Blogs.jsx
+++ 18-Blogs-Website(Project-3)/src/components/Blogs.jsx
@@ -1,7 +1,6 @@
 import { useContext } from "react"
 import { AppContext } from "../context/AppContextProvider"
 import Spinner from "./Spinner"
-import Card from "./Card"
 
 function Blogs () {
     const {loading, posts} = useContext(AppContext)
@@ -11,7 +10,35 @@ function Blogs () {
         (<Spinner/>) :
          (posts.length === 0 ?
           (<div> <h3>No Posts Found...</h3></div>) :
-           (posts.map ( (post) => (<Card/>)))
+           (posts.map ( (post) => {
+            return (
+                <div className="card" key={post.id}>
+                    <div className="title">
+                        <p>{post.title}</p>
+                    </div>
+
+
+                    <div className="author">
+                        <p>By <span>{post.author}</span>on <span>{post.category}</span></p>
+                    </div>
+
+
+                    <div className="date">
+                        <p>Posted on {post.date}</p>
+                    </div>
+
+                    <div className="content">
+                        <p>{post.content}</p>
+                    </div>
+
+                    <div className="tags">
+                        {post.tags.map ( (tag, index) => {
+                            return <span key={index}>{`#${post.tag}`}</span>
+                        } )}
+                    </div>
+                </div>
+            )
+           }))
            )}
     </div>
     )
