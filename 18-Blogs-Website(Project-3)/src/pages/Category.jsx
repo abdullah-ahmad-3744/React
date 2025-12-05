1b4c4b630cb56cfe6b21bb77e87ecde5e18e8367
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-05 15:59:00 +0500

    Category page created

--- 18-Blogs-Website(Project-3)/src/pages/Category.jsx
+++ 18-Blogs-Website(Project-3)/src/pages/Category.jsx
@@ -1,7 +1,23 @@
+import { useLocation, useNavigation } from "react-router-dom"
+import Header from "../components/Header"
+import Blogs from '../components/Blogs'
+import Pagination from '../components/Pagination'
+
 function Category () {
+    const navigation = useNavigation()
+    const location = useLocation()
+    const category = location.pathname.split('/').at(-1)
     return (
         <div className="cateogry">
-            <h1>Category</h1>
+            <Header />
+            <div className="caterogy-page-content">
+
+                <button  onClick={() =>navigation(-1) }>Back</button>
+
+                <h2>Blogs on <span>{Category}</span> </h2>
+            </div>
+            <Blogs/>
+            <Pagination />
         </div>
     )
 }
