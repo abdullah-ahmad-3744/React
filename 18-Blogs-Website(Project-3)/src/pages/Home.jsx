a039ab35f6b2c8dc342517bbb379a6251faac06b
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-05 12:13:16 +0500

    Home and Blog component (based on tag) handleded

--- 18-Blogs-Website(Project-3)/src/pages/Home.jsx
+++ 18-Blogs-Website(Project-3)/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Blogs from "../components/Blogs"
 import Header from "../components/Header"
 import Pagination from "../components/Pagination"
-
+import './Home.css'
 function Home() {
     return (
         <div className="home">
--- 18-Blogs-Website(Project-3)/src/pages/Tag.jsx
+++ 18-Blogs-Website(Project-3)/src/pages/Tag.jsx
@@ -1,7 +1,23 @@
+import { useLocation, useNavigation } from "react-router-dom"
+import Header from "../components/Header"
+import Blogs from "../components/Blogs"
+import Pagination from "../components/Pagination"
+
 function Tag () {
+    const navigation = useNavigation()
+    const location = useLocation()
+    const tag = location.pathname.split('/').at(-1)
     return (
         <div className="tag">
-            <h1>Tag</h1>
+            <Header />
+            <div className="tag-page-content">
+
+                <button onClick={() => navigation(-1)}>Back</button>
+                <h2>Blogs Tagged<span>#{tag}</span></h2>
+            </div>
+
+            <Blogs />
+            <Pagination />
         </div>
     )
 }
