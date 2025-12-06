6dec59946e94854df9e4536d8d33828180a855aa
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-06 11:26:04 +0500

    Shopping cart project using React

---
+++ 20-Shopping Cart (Project-4)/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="main-container">
+      <h1>Shopping Cart Project using Redux</h1>
+    </div>
+  )
+}
+
+export default App
