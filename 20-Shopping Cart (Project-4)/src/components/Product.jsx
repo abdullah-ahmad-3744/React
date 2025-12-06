b44ec0e6335b9a25782e0ced6c8eea64efb8ff70
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-06 14:42:47 +0500

    Product component created and products or cards are rendering on UI

--- 20-Shopping Cart (Project-4)/src/components/Product.jsx
+++ 20-Shopping Cart (Project-4)/src/components/Product.jsx
@@ -1,7 +1,33 @@
-function Product () {
+import { useState } from 'react'
+import './Product.css'
+function Product ({post}) {
+    const [selected, setSelected] = useState(false)
+    console.log(' ')
     return (
         <div className="product">
-            <h1>Product </h1>
+
+
+            <div className="card-title">
+                <p>{post.title}</p>
+            </div>
+
+            <div className="description">
+                <p>{post.description}</p>
+            </div>
+
+            <div className="card-image">
+                <img src={post.image} alt=""  />
+            </div>
+
+            <div className="price">
+                <p>{post.price}</p>
+            </div>
+
+            <div className="buttons">
+                {selected ? 
+                (<><button>Remove from Cart</button></>) : 
+                (<><button>Add To Cart</button></>)}
+            </div>
         </div>
     )
 }
