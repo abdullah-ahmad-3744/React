fa7f3e272744992889298cf70990cb2377e4e462
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-06 14:26:57 +0500

    Home page created

--- 20-Shopping Cart (Project-4)/src/pages/Home.jsx
+++ 20-Shopping Cart (Project-4)/src/pages/Home.jsx
@@ -1,8 +1,42 @@
-function Home () {
+import { useEffect, useState } from "react";
+import Spinner from "../components/Spinner";
+import Product from "../components/Product";
+
+function Home() {
     const apiUrl = 'https://fakestoreapi.com/products';
-    return (
+    const [loading, setLoading] = useState(false)
+    const [posts , setPosts] = useState([])
+
+
+     async function fetchApiData() {
+        setLoading(true)
+
+            try {
+                const apiResponse = await fetch(apiUrl);
+                const data = await apiResponse.json()
+                setPosts(data)
+            } catch (error) {
+                console.log("Error in Fetching data...", error)
+                setPosts([])
+            }
+            setLoading(false)
+        }
+
+        useEffect ( () => {
+            fetchApiData()
+        }, [])
+        return (
         <div className="home">
-            <h1>This is Home Page</h1>
+            {loading ? (<Spinner/>) :
+             posts.length > 0 ? 
+             (<div>
+                {posts.map ((post) => {
+                    return (
+                        <Product key={post.id}  post ={post}/>
+                    )
+                })}
+             </div>) : 
+             (<div> <p>No Post Found</p></div>)}
         </div>
     )
 }
