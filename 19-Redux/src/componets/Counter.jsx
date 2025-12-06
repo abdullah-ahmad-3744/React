895b60aa3a305232a6606e053ce81b6930905334
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2025-12-06 11:06:16 +0500

    Styling Added for Counter App

--- 19-Redux/src/componets/Counter.jsx
+++ 19-Redux/src/componets/Counter.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux"
 import { decrement, increment } from "../redux/slice/CounterSlice"
+import './Counter.css'
 
 function Counter () {
     const  count = useSelector((state) => state.counter.value)
